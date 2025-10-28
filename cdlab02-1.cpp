caffb3074467d856ac6a9497e2a6fe850af7254b
galib-horibol <galib.horibol@gmail.com>
2025-10-28 12:14:59 +0600

    Add files via upload

---
+++ cdlab02-1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void commentDetect()
+{
+    string input;
+
+    cout << "Enter text (use multiple lines if needed, and press ENTER + CTRL+Z (Windows) or CTRL+D (Linux) to finish):\n";
+
+    string line;
+    while (getline(cin, line))
+    {
+        input = input + line + '\n';
+    }
+
+    bool found = false;
+
+    for (int i = 0; i < input.length() - 1; i++)
+    {
+        if (input[i] == '/' && input[i + 1] == '/')
+        {
+            found = true;
+            cout << "\nSingle-line Comment Found:\n";
+            i += 2; 
+            while (i < input.length() && input[i] != '\n')
+            {
+                cout << input[i];
+                i++;
+            }
+            cout << endl;
+        }
+
+        else if (input[i] == '/' && input[i + 1] == '*')
+        {
+            found = true;
+            cout << "\nMultiline Comment Found:\n";
+            i += 2; 
+            while (i < input.length() - 1)
+            {
+                if (input[i] == '*' && input[i + 1] == '/')
+                {
+                    cout << "\n(Comment ends here)\n";
+                    i++;
+                    break;
+                }
+                cout << input[i];
+                i++;
+            }
+        }
+    }
+
+    if (!found)
+        cout << "\nNo comment found.\n";
+}
+
+int main()
+{
+    commentDetect();
+    return 0;
+}
